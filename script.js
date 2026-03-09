e8ad95a36a3ff7966055866ddbbe1c525f49444f
Yazan Al-Qhtany <65427244+n9dl@users.noreply.github.com>
2026-03-09 19:21:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,27 @@ navLinks.forEach(link => {
         hamburgerIcon.classList.add('fa-bars');
     });
 });
+
+// سحب البيانات من لوحة التحكم وعرضها في الموقع
+fetch('data/home.json')
+  .then(response => response.json())
+  .then(data => {
+    document.querySelectorAll('[data-cms-bind]').forEach(el => {
+      const key = el.getAttribute('data-cms-bind');
+      if (data[key]) {
+        // إذا كان العنصر صورة
+        if (el.tagName === 'IMG') {
+          el.src = data[key];
+        } 
+        // إذا كان العنصر رابط تواصل (يغير الرابط مو الأيقونة)
+        else if (key.includes('link')) {
+          el.href = data[key];
+        } 
+        // للنصوص العادية
+        else {
+          el.innerHTML = data[key];
+        }
+      }
+    });
+  })
+  .catch(err => console.log('الموقع يعرض النسخة الأساسية (لا توجد تعديلات بعد)'));
